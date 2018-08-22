6f6f8b33b04ed8939087b2b72e20b1cce5221c02
Bekrenev Dmitry <dbekrenev@eastbanctech.com>
2018-08-22 18:37:45 +0700

    Add settings for maven and fix class name

---
+++ check-mvn-settings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+m2settings=~/.m2/settings.xml
+
+if [ ! -f "$m2settings" ]; then
+ echo "Maven settings not found. Create settings for nexus"
+
+cat > $m2settings <<- EOF
+ <?xml version="1.0" encoding="UTF-8"?> 
+ <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"> 
+  <servers>
+   <server>
+    <id>nexus</id>
+    <username>\${nexus.user}</username>
+    <password>\${nexus.password}</password>
+   </server>
+  </servers> 
+ </settings>
+EOF
+ 
+fi
