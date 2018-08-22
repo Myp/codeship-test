a133acf770bc7c0dcf9155ad5845c7d539403da7
Bekrenev Dmitry <dbekrenev@eastbanctech.com>
2018-08-22 18:47:57 +0700

    Add Publish to nexus

--- publish-release.sh
+++ publish-release.sh
@@ -3,7 +3,17 @@
 
 if [[ "$CI_BRANCH" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
  echo "Publish release $CI_BRANCH"
- curl https://20669c2b.ngrok.io/publish/release/$CI_BRANCH 
+ mvn deploy:deploy-file -DgroupId=test.testcompany \
+                        -DartifactId=test-file \
+                        -Dversion=$CI_BRANCH \
+                        -DgeneratePom=true \
+                        -Dpackaging=jar \
+                        -DrepositoryId=nexus \
+                        -Durl=$NEXUS_URL \
+                        -Dfile=test-codeship.jar \
+                        -Dnexus.user=$NEXUS_USER \
+                        -Dnexus.password=$NEXUS_PASSWORD
+ 
 else  
  echo "It's just commit. Nothing to do"
 fi 
